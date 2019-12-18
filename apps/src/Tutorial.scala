e0c193a3f46a2458761fb6e332f9c4a0ffad019d
Ruizhe <vincentzhaorz@gmail.com>
2019-12-18 17:47:44 +0000

    Reviewed basic syntax

--- apps/src/Tutorial.scala
+++ apps/src/Tutorial.scala
@@ -16,3 +16,52 @@ import spatial.dsl._
         println(r"Result is ${getArg(y)}")
     }
 }
+
+@spatial object HelloSpatialMemory extends SpatialApp {
+
+    def main(args: Array[String]): Void = {
+
+        val x = ArgIn[Int]
+        val y = ArgIn[Int]
+
+        setArg(x, args(0).to[Int])
+        setArg(y, args(1).to[Int])
+
+
+        Accel {
+            val s = SRAM[Int](16, 32)
+            val r = Reg[Int]
+
+            Foreach(16 by 1, 32 by 1) { (i, j) =>
+                s(i, j) = i + j
+            }
+
+            r := s(x, y)
+
+            println(r"Value of SRAM at (${x.value}, ${y.value}) is ${r.value}")
+        }
+    }
+
+}
+
+@spatial object HelloSpatialDRAM extends SpatialApp {
+
+    def main(args: Array[String]): Void = {
+        val d = DRAM[Int](16)
+
+        val data = Array.fill[Int](16)(0)
+        setMem(d, data)
+        
+        Accel {
+            val s = SRAM[Int](16)
+
+            s load d(0::16)
+
+            Foreach(16 by 1){i => s(i) = s(i) + 1 }
+
+            d(0::16) store s
+        }
+
+        printArray(getMem(d), "Result: ")
+    }
+}
