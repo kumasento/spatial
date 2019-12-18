80fab0d9974b28a83ae82cf4227cda5654f5753b
Vincent Zhao <vincentzhaorz@gmail.com>
2019-12-18 18:06:08 +0000

    Added InnerProductPlain

--- apps/src/Tutorial.scala
+++ apps/src/Tutorial.scala
@@ -65,3 +65,41 @@ import spatial.dsl._
         printArray(getMem(d), "Result: ")
     }
 }
+
+@spatial object InnerProductPlain extends SpatialApp {
+  def main(args: Array[String]): Void = {
+    type T = FixPt[TRUE, _24, _8]
+
+    val len = 64
+
+    val vec1 = Array.tabulate[T](len){ i => i.to[T] }
+    val vec2 = Array.tabulate[T](len){ i => (len - i).to[T] }
+
+    val d1 = DRAM[T](len)
+    val d2 = DRAM[T](len)
+
+    setMem(d1, vec1)
+    setMem(d2, vec2)
+
+    val x = ArgOut[T]
+
+    Accel {
+      val s1 = SRAM[T](len)
+      val s2 = SRAM[T](len)
+
+      s1 load d1
+      s2 load d2
+
+      x := Reduce(Reg[T](0))(len by 1) { i => 
+        s1(i) * s2(i)
+      }{ _ + _ }
+
+    }
+
+    val gold = vec1.zip(vec2){ _ * _ }.reduce{ _ + _ }
+
+    assert(gold == getArg(x), r"Expected ${gold}, got ${getArg(x)}")
+
+    println(r"Result: ${getArg(x)}")
+  }
+}
